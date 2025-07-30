22ebfc08a96f7a6111cc2ee0ef9242a26478162a
Huan Zhao <449162701@qq.com>
2025-07-29 20:22:57 -0500

    Update factory.py

--- open_clip/factory.py
+++ open_clip/factory.py
@@ -40,7 +40,7 @@ def locate_file(filename, search_paths=None):
     print("===current dir is ===", os.getcwd())
     if search_paths is None:
         # 你可以根据实际情况添加更多路径或挂载点
-        search_paths = ["/outputs/model_cache"] # os.path.expanduser("~"), "/inputs/repo", "/", 
+        search_paths = ["/root/.cache"] # os.path.expanduser("~"), "/inputs/repo", "/", 
     
     for base_path in search_paths:
         print("base_path:", base_path)
@@ -243,19 +243,20 @@ def create_model(
                 # print(os.path.exists("Diffusion/laion/laion2b_s12b_b42k/open_clip_pytorch_model.bin"))
                 # print(os.path.exists("/Diffusion/laion/laion2b_s12b_b42k/open_clip_pytorch_model.bin"))
                 # print(os.path.exists("./Diffusion/laion/laion2b_s12b_b42k/open_clip_pytorch_model.bin"))
-                print(os.path.exists("diffusion-laion-laion2b_s12b_b42k/open_clip_pytorch_model.bin"))
+                # print(os.path.exists("diffusion-laion-laion2b_s12b_b42k/open_clip_pytorch_model.bin"))
+                print(os.path.exists("/root/.cache/open_clip_pytorch_model.bin"))
                 
                 
                 # print(os.path.exists("/Gaussian_Shading_ECC/Diffusion/laion/laion2b_s12b_b42k/open_clip_pytorch_model.bin"))
                 # 在你的逻辑里这样用：
-                # filename = "open_clip_pytorch_model.bin"
-                # checkpoint_path = locate_file(filename)
-                # print(f"======[INFO] find pretrained at =====：{checkpoint_path}")
+                filename = "open_clip_pytorch_model.bin"
+                checkpoint_path = locate_file(filename)
+                print(f"======[INFO] find pretrained at =====：{checkpoint_path}")
                 
 
 
                 # checkpoint_path = '/public/yangzijin/Diffusion/laion/laion2b_s12b_b42k/open_clip_pytorch_model.bin'
-                checkpoint_path = '/inputs/pretrained/open_clip_pytorch_model.bin'
+                checkpoint_path = '/root/.cache/open_clip_pytorch_model.bin'
                 
 
 
